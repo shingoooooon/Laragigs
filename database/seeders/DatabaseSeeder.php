24e396ce4cc8212777a50cf99391b14ffc4eecc3
shingo kubomura <skksb75@gmail.com>
2022-10-12 14:49:45 +0700

    Initial setting

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Listing;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+         User::factory(5)->create();
+         Listing::factory(5)->create();
+    }
+}
